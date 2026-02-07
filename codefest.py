5735f8cab021014c0a64fab364eecc9f55b1c946
Aishu12345632 <123235413+Aishu12345632@users.noreply.github.com>
2026-02-07 11:29:54 +0530

    Fix typo in air quality advice and data structure
    Corrected a typo in the advice message and fixed a missing comma in the data dictionary.

--- codefest.py
+++ codefest.py
@@ -11,5 +11,48 @@ def aqi_parameters(aqi):
     elif aqi<=200:
         return "Air quality is manageable, but indoor workouts may feel better."
     else:
-        return "Masks help, especially near traffic-heavy aishus."
+        return "Masks help, especially near traffic-heavy areas."
+
+@app.route("/", methods=["GET","POST"])
+
+    def home():
+        data = none
+    
+        if request.method == "POST":
+            city = request.form["city"]
+
+            # loctions into longitude and latititude
+            geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}"
+            geo_data = requests.get(geo_url).json()
+
+            if geo_data:
+                lat = geo_data[0]["lat"]
+                lon = geo_data[0]["lon"]
+                
+                aqi_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}"
+                aqi_data=requests.get(aqi_url).json()
+                aqi = aqi_data["list"][0]["main"]["aqi"]
+
+                weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
+                weather = requests.get(weather_url).json()
+
+                category, advice = classify_aqi()
+
+                data = {
+                    "city": city,
+                    "aqi": aqi,
+                    "category": category,
+                    "advice": advice,
+                    "temp": weather["main"]["temp"],
+                    "humidity": weather["main"]["humidity"]
+                    "wind": weather["wind"]["speed"]
+                }
+
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
     
